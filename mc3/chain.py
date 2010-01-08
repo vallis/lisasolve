852b27768fec51cd97303fac43757ad500452c04
vallis <vallis@1d4be006-9413-9d82-1de5-0ac26b53220f>
2010-01-08 04:09:15 +0000

    Use temperature, choose which parameters to save.

--- mc3/chain.py
+++ mc3/chain.py
@@ -1,38 +1,43 @@
-import math
+import math, copy
 
 class chain(object):
-    def __init__(self,model,proposal,stepper,data):
+    def __init__(self,model,proposal,stepper,data,temperature=1.0):
         self.model    = model
         self.proposal = proposal
         self.stepper  = stepper
         self.data     = data
         
-        self.state = None
+        self.temperature = temperature
         
+        self.state = None
         self.samples = []
     
     def run(self,iterations):
         if len(self.samples) == 0:
-            self.state = self.model.initstate            
+            self.state = self.model.initstate
             self.state.logL = self.data.logL(self.model.signal(self.state),self.state)
-        
+                
         for iter in range(iterations):
             newstate = self.proposal.propose(self.state)        
             newstate.logL = self.data.logL(self.model.signal(newstate),newstate)
+            # should we cache the computation of the signal? It's called twice...
             
-            self.state = self.stepper.step(self.state,newstate)
+            self.state = self.stepper.step(self.state,newstate,self.temperature)
             
-            pars =  [self.state[par] for par in self.model.parameters + self.model.computed]
-            pars += [self.state.prior, self.state.logL, self.state.p]
+            for par in self.model.computed:
+                if par not in self.state and hasattr(par,'compute'):
+                    self.state[par] = par.compute(self.model.signal(self.state))
+            
+            pars =  [self.state[par] for par in self.model.saved]
+            pars += [self.state.prior, self.state.logL]
             
             self.samples.append(pars)
-    
-    def getpar(self,par):
-        if par in self.model.parameters:
-            ind = self.model.parameters.index(par)
-        elif par in self.model.computed:
-            ind = len(self.model.parameters) + self.model.computed.index(par)
-        else:
-            return []
         
-        return [sample[ind] for sample in self.samples]
+    def getpar(self,par):
+        # see if we have stored the parameter...
+        try:
+            ind = (self.model.saved + ['prior','logL']).index(par)
+            return [sample[ind] for sample in self.samples]
+        except:
+            return []
+    
