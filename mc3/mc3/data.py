2322ab6b9f036152f68bcdec4c0e252677a27ab3
vallis <vallis@1d4be006-9413-9d82-1de5-0ac26b53220f>
2010-06-21 21:59:36 +0000

    Add stripargument and select

--- mc3/mc3/data.py
+++ mc3/mc3/data.py
@@ -1,12 +1,8 @@
+from utility import stripargument
+
 class data(object):
-    # a little magic to allow logL functions with only one parameter...
-    # this is fun, but could it be done more cleanly?
-    
     def setlogL(self,func):
-        if func.func_code.co_argcount == 1:
-            self.__dict__['logL'] = lambda signal, state: func(signal)
-        else:
-            self.__dict__['logL'] = func
+        self.__dict__['logL'] = stripargument(func)
     
     def getlogL(self):
         return self.__dict__['logL']
--- mc3/mc3/utility.py
+++ mc3/mc3/utility.py
@@ -1,5 +1,19 @@
 def stripargument(function):
+    """Allow calling a one-argument function with two arguments."""
+    
     if function.func_code.co_argcount == 1:
         return (lambda x,y: function(x))
     else:
         return function
+
+def select(parameters,selected):
+    if not isinstance(selected,(tuple,list)):
+        selected = [selected]
+    
+    if isinstance(parameters,(tuple,list)):
+        return [par for par in parameters if par in selected]
+    elif isinstance(parameters,dict):
+        return dict((key,parameters[key]) for key in selected)
+    else:
+        return TypeError
+
