858d176d5b4f941b8055a5eff0fcbd890eda75d7
vallis <vallis@1d4be006-9413-9d82-1de5-0ac26b53220f>
2010-06-21 22:00:41 +0000

    Covariant Gaussian steps

--- mc3/mc3/proposal.py
+++ mc3/mc3/proposal.py
@@ -1,22 +1,49 @@
-import random, math
+import math
+
+import numpy
 
 from state import state
 
-# need to keep the random state somehow
-# perhaps it can be done by the base class
+# TO DO: need to keep the random state somehow
+#        perhaps it can be done by the base class
 
 class proposal(object):
-    pass
+    def __init__(self,func=None):
+        self.func = func
+    
+    def propose(self,current):
+        return self.func(current)
+        
 
 class Gaussian(proposal):
+    def __init__(self,scale=None):
+        self.scale = scale
+        self.norm  = None
+    
     def propose(self,current):
-        proposed = state(current.model)
+        x = current.asarray()
         
-        norm = math.sqrt(len(current.model.parameters))
-        for par in current.model.parameters:
-            proposed[par] = current[par] + random.normalvariate(0,1) / norm
+        # the first time we run, initialize the proposal normalization matrix
+        if self.norm == None:
+            if self.scale == None:
+                # by default, do no scaling
+                self.scale = numpy.ones(len(x),'d')
+            else:
+                # otherwise use the vector or matrix passed at initialization
+                self.scale = current.fromarray(self.scale)
             
-            if hasattr(par,'renormalize'):
-                proposed[par] = par.renormalize(proposed[par])
+            # if we're carrying a vector, use it as a diagonal scaling matrix
+            # if we're carrying a matrix, treat it as a covariance matrix,
+            #    and use it square root (Cholesky) to produce correlated Gaussian displacements 
+            self.norm = (numpy.diag(self.scale) if len(self.scale.shape) == 1
+                                                else numpy.linalg.cholesky(self.scale)) / math.sqrt(len(x))
         
-        return proposed
+        y = x + numpy.dot(self.norm,numpy.random.randn(len(x)))
+        
+        return current.model.state(y)
+    
+
+class fromprior(proposal):
+    def propose(self,current):
+        return current.model.state([p.random() for p in current.model.parameters])
+    
