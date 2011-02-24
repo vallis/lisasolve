0f797bd03044dc0cea13e39379ea1a3a43ff01fe
m.vallis <m.vallis@1d4be006-9413-9d82-1de5-0ac26b53220f>
2011-02-24 01:51:59 +0000

    Various TDI operations, SNR models, log-likelihood

--- mc3/mc3lisa/tdi.py
+++ mc3/mc3lisa/tdi.py
@@ -16,8 +16,9 @@ class TDIf(object):
             self.Af, self.Ef, self.Tf = aet
         elif xyz != None:
             self.Af, self.Ef, self.Tf = AET(*xyz)
+            self.Xf = xyz[0]                        # keep also X (temporary?)
         
-        self._Sae, self._St = None, None
+        self._Sae, self._St, self._Sx = None, None, None
     
     
     def pad(self,leftpad=1,rightpad=1):
@@ -25,9 +26,13 @@ class TDIf(object):
         self.Ef = self.Ef.pad(leftpad,rightpad)
         self.Tf = self.Tf.pad(leftpad,rightpad)
         
-        self._Sae, self._St = None, None
+        self.Xf = self.Xf.pad(leftpad,rightpad)
+        
+        self._Sae, self._St, self._Sx = None, None, None
+        
+        return self
     
-    
+
     # TO DO: memoize the properties?    
     @property
     def Sae(self):
@@ -39,6 +44,17 @@ class TDIf(object):
         if self._St  == None: self._St  = noisepsd_T(self.Tf)
         return self._St
     
+    @property
+    def Sx(self):
+        if self._Sx  == None: self._Sx  = noisepsd_X(self.Xf)
+        return self._Sx
+    
+    @property
+    def kmin(self):
+        return self.Af.kmin
+    
+    def __len__(self):
+        return len(self.Af)
     
     def __add__(self,other):
         return TDIf(aet=(self.Af + other.Af,self.Ef + other.Ef,self.Tf + other.Tf))
@@ -52,11 +68,27 @@ class TDIf(object):
         else:
             return TDIf(aet=(self.Af/other,self.Ef/other,self.Tf/other))
     
+    def __iadd__(self,other):
+        self.Af += other.Af; self.Ef += other.Ef; self.Tf += other.Tf
     
-    def normsq(self):
-        return (4.0 / self.Af.df) * ( N.sum(N.abs(self.Af)**2 / self.Sae) + 
-                                      N.sum(N.abs(self.Ef)**2 / self.Sae) +
-                                      N.sum(N.abs(self.Tf)**2 / self.St ) )
+    def __isub__(self,other):
+        self.Af -= other.Af; self.Ef -= other.Ef; self.Tf -= other.Tf
+    
+    def normsq(self,noisepsd = None,extranoise = [0,0,0]):
+        if noisepsd == None:
+            return (4.0 / self.Af.df) * ( N.sum(N.abs(self.Af)**2 / (self.Sae + extranoise[0])) + 
+                                          N.sum(N.abs(self.Ef)**2 / (self.Sae + extranoise[1])) +
+                                          N.sum(N.abs(self.Tf)**2 / (self.St  + extranoise[2])) )
+        else:
+            return (4.0 / self.Af.df) * ( N.sum(N.abs(self.Af)**2 / noisepsd[0]) + 
+                                          N.sum(N.abs(self.Ef)**2 / noisepsd[1]) +
+                                          N.sum(N.abs(self.Tf)**2 / noisepsd[2]) )
+    
+    def normsqx(self,noisepsd = None):
+        if noisepsd == None:
+            return (4.0 / self.Xf.df) * ( N.sum(N.abs(self.Xf)**2 / (self.Sx)) )
+        else:
+            return (4.0 / self.Xf.df) * ( N.sum(N.abs(self.Xf)**2 / noisepsd) )
     
     def dotprod(self,other):
         return (4.0 / self.Af.df) * N.real( N.sum(N.conj(self.Af) * other.Af / self.Sae) +
@@ -64,14 +96,11 @@ class TDIf(object):
                                             N.sum(N.conj(self.Tf) * other.Tf / self.St ) )    
     
     def logL(self,other):
-        delta = TDIf(aet=(self.Af.rsub(other.Af),
-                          self.Ef.rsub(other.Ef),
-                          self.Tf.rsub(other.Tf)))
-        # noises are the same...
-        delta._Sae, delta._St = self._Sae, self._St
-        
-        return -0.5 * delta.normsq()
+        return -0.5 * (4.0 / self.Af.df) * ( N.sum(N.abs(self.Af.rsub(other.Af))**2 / self.Sae) +
+                                             N.sum(N.abs(self.Ef.rsub(other.Ef))**2 / self.Sae) +
+                                             N.sum(N.abs(self.Tf.rsub(other.Tf))**2 / self.St ) )
     
+
 
 
 def SNR(state):
@@ -97,6 +126,49 @@ def fisher(center,scale):
     return prods
 
 
+defaultmodel = 'lisareq'
+
+def lisanoises(f,noisemodel=None):
+    if noisemodel == None: noisemodel = defaultmodel
+    
+    if   noisemodel == 'mldc':
+        Spm = 2.5e-48 * (1.0 + (f/1.0e-4)**-2) * f**(-2)
+        Sop = 1.8e-37 * f**2
+    elif noisemodel == 'mldc-nominal':
+        Spm = 2.53654e-48 * (1.0 + (f/1.0e-4)**-2) * f**(-2)    # 3e-15 m/s^2/sqrt(Hz)
+        Sop = 1.75703e-37 * f**2                                # 2e-11 m/sqrt(Hz)
+    elif noisemodel == 'lisareq':
+        Spm = 2.53654e-48 * (1.0 + (f/1.0e-4)**-1) * (1.0 + (f/0.008)**4) * f**(-2)
+        Sop = 1.42319e-37 * (1.0 + (f/0.002)**-4) * f**2
+    
+    return Spm, Sop
+
+def lisanoise(f,noisemodel=None):
+    if noisemodel == 'cutler':
+        return (20.0/3.0)*(9.18e-52 * f**-4 + 1.59e-41 + 9.18e-38 * f**2)
+    
+    # otherwise use the LISA requirements model
+    c = 299792458; L = 16.6782 * c  # note: it's really 16.67 in the requirements
+    
+    Sa  = 3e-15 * N.sqrt(1.0 + (f/1.0e-4)**-1) * N.sqrt(1.0 + (f/0.008)**4)
+    Sac = Sa * 2.0 / (2.0 * math.pi * f)**2
+    
+    So  = 18e-12 * N.sqrt(1 + (f/0.002)**-4)
+    
+    ft = c / (2.0 * L)
+    T2 = 1.0 + (f/(0.41 * ft))**2
+    
+    return (20.0/3.0) * T2 * (Sac**2 + So**2) / L**2
+
+def simplesnr(f,h,i=None,years=1,noisemodel=None):
+    if i == None:
+        h0 = h * math.sqrt(16.0/5.0)    # rms average over inclinations
+    else:
+        h0 = h * math.sqrt((1 + math.cos(i)**2)**2 + (2*math.cos(i))**2)        
+    
+    return h0 * math.sqrt(years * 365.25*24*3600) / math.sqrt(lisanoise(f,noisemodel))
+
+
 includewd = False
 
 # from lisatools makeTDIsignal-synthlisa2.py
@@ -106,8 +178,7 @@ def noisepsd_X(frequencydata):
     L  = 16.6782
     x = 2.0 * math.pi * L * f
     
-    Spm = 2.5e-48 * (1.0 + (f/1.0e-4)**-2) * f**(-2)
-    Sop = 1.8e-37 * f**2
+    Spm, Sop = lisanoises(f)
     
     Sx = 16.0 * N.sin(x)**2 * (2.0 * (1.0 + N.cos(x)**2) * Spm + Sop)
     # Sxy = -4.0 * N.sin(2*x) * N.sin(x) * (Sop + 4.0*Spm)
@@ -128,8 +199,7 @@ def noisepsd_AE(frequencydata):
     L  = 16.6782
     x = 2.0 * math.pi * L * f
     
-    Spm = 2.5e-48 * (1.0 + (f/1.0e-4)**-2) * f**(-2)
-    Sop = 1.8e-37 * f**2
+    Spm, Sop = lisanoises(f)
     
     Sa = 8.0 * N.sin(x)**2 * (2.0 * Spm * (3.0 + 2.0*N.cos(x) + N.cos(2*x)) +
                               Sop * (2.0 + N.cos(x)))
@@ -153,8 +223,7 @@ def noisepsd_T(frequencydata):
     L  = 16.6782
     x = 2.0 * math.pi * L * f
     
-    Spm = 2.5e-48 * (1.0 + (f/1.0e-4)**-2) * f**(-2)
-    Sop = 1.8e-37 * f**2
+    Spm, Sop = lisanoises(f)
     
     St = 16.0 * Sop * (1.0 - N.cos(x)) * N.sin(x)**2 + 128.0 * Spm * N.sin(x)**2 * N.sin(0.5*x)**4
     
@@ -187,3 +256,8 @@ def noisepsd_T(frequencydata):
 # then ret(f_i) must be h(f_i) * df, and therefore the SNR integral is
 #
 # 4 Re int |h(f)|^2 / Sn(f) df = 4 sum |ret(f_i) / df|^2 / Sn(f_i) df = (4/df) sum |ret(f_i)|^2 / Sn(f_i) 
+#
+# now how's h(f_i) related to S_h(f)? I have
+#
+# int_0^\infty S_h(f) = (1/T) \int_0^T |h(t)|^2 dt = (2/T) \int_0^\infty |h(f)|^2 df
+# so S_h(f_i) = (2/T) |h(f_i)|^2 = (2/T) |ret(f_i)|^2 / df^2 = (2/df) |ret(f_i)^2|
