386d8d0a65188c9f70f144f6c53e3e566f23e419
m.vallis <m.vallis@1d4be006-9413-9d82-1de5-0ac26b53220f>
2011-04-22 20:53:15 +0000

    Compute Fisher

--- trunk/fastsource/fastbinary/examples/test-fastbinary.py
+++ trunk/fastsource/fastbinary/examples/test-fastbinary.py
@@ -1,27 +1,38 @@
 import math
+
+import numpy as N
+
 import mc3lisa
 import FastBinary
 
 fastbin = FastBinary.FastGalacticBinary()
+def makebinary(par):
+    return mc3lisa.tdi.TDIf(xyz=fastbin.onefourier(vector=par,T=6.2914560e7,dt=15))
 
 # -- binary one
 
 # parameters: f [Hz], fdot [Hz/s], lat [-pi/2,pi/2], lon [0,2pi], A [strain], inc [0,pi], psi [0,pi], phi [0,2pi]
 par1 = [1e-3, 0, 0.25*math.pi, 0.25*math.pi, 1e-22, 0.45*math.pi, 0.35*math.pi, 1.75*math.pi]
 # T and dt are the default values; best to have T/dt = power of 2
-tdi1 = mc3lisa.tdi.TDIf(xyz=fastbin.onefourier(vector=par1,T=6.2914560e7,dt=15))
+tdi1 = makebinary(par1)
 
 # -- binary two
 
 year = 3.15581498e7
 par2 = [1e-3 + 3.5/year, 0, -0.4*math.pi, 1.65*math.pi, 7e-23, 0.15*math.pi, 0.65*math.pi, 0.25*math.pi]
-tdi2 = mc3lisa.tdi.TDIf(xyz=fastbin.onefourier(vector=par2))
+tdi2 = makebinary(par2)
 
 # -- A-E-T SNRs and match
 
 print "SNR(h1) = ", math.sqrt(tdi1.normsq())
 print "SNR(h2) = ", math.sqrt(tdi2.normsq())
 print "(h1,h2) = ", math.sqrt(tdi1.dotprod(tdi2))
+
+# make a Fisher matrix...
+fmat = mc3lisa.fisher(par1,[1/year,1/(year**2),0.01,0.01,1e-23,0.1,0.1,0.1],makebinary)
+
+# print parameter errors
+print N.sqrt(N.diag(N.linalg.inv(fmat)))
 
 try:
     import matplotlib.pyplot as P
